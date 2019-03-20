b7a97a234482de6188070f5a2010204f00d22149
sshya <31662008+sshya@users.noreply.github.com>
2019-03-20 14:18:29 -0400

    Add files via upload

--- time/time.js
+++ time/time.js
@@ -1,35 +1,30 @@
-d3.time = {};
+var offset = 0;
 
-var d3_time = Date,
-    d3_time_daySymbols = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-function d3_time_utc() {
-  this._ = new Date(arguments.length > 1
-      ? Date.UTC.apply(this, arguments)
-      : arguments[0]);
-}
-
-d3_time_utc.prototype = {
-  getDate: function() { return this._.getUTCDate(); },
-  getDay: function() { return this._.getUTCDay(); },
-  getFullYear: function() { return this._.getUTCFullYear(); },
-  getHours: function() { return this._.getUTCHours(); },
-  getMilliseconds: function() { return this._.getUTCMilliseconds(); },
-  getMinutes: function() { return this._.getUTCMinutes(); },
-  getMonth: function() { return this._.getUTCMonth(); },
-  getSeconds: function() { return this._.getUTCSeconds(); },
-  getTime: function() { return this._.getTime(); },
-  getTimezoneOffset: function() { return 0; },
-  valueOf: function() { return this._.valueOf(); },
-  setDate: function() { d3_time_prototype.setUTCDate.apply(this._, arguments); },
-  setDay: function() { d3_time_prototype.setUTCDay.apply(this._, arguments); },
-  setFullYear: function() { d3_time_prototype.setUTCFullYear.apply(this._, arguments); },
-  setHours: function() { d3_time_prototype.setUTCHours.apply(this._, arguments); },
-  setMilliseconds: function() { d3_time_prototype.setUTCMilliseconds.apply(this._, arguments); },
-  setMinutes: function() { d3_time_prototype.setUTCMinutes.apply(this._, arguments); },
-  setMonth: function() { d3_time_prototype.setUTCMonth.apply(this._, arguments); },
-  setSeconds: function() { d3_time_prototype.setUTCSeconds.apply(this._, arguments); },
-  setTime: function() { d3_time_prototype.setTime.apply(this._, arguments); }
+exports.local = function(year, month, day, hours, minutes, seconds, milliseconds) {
+  var date = new Date();
+  date.setFullYear(year, month, day);
+  date.setHours(hours || 0, offset + (minutes || 0), seconds || 0, milliseconds || 0);
+  return date;
 };
 
-var d3_time_prototype = Date.prototype;
+exports.utc = function(year, month, day, hours, minutes, seconds, milliseconds) {
+  var date = new Date();
+  date.setUTCFullYear(year, month, day);
+  date.setUTCHours(hours || 0, minutes || 0, seconds || 0, milliseconds || 0);
+  return date;
+};
+
+exports.zone = function(tzOffset, scope) {
+  return function() {
+    var o = Date.prototype.getTimezoneOffset;
+    try {
+      // Note: assumes the dates are not in DST.
+      offset = -tzOffset - new Date(0).getTimezoneOffset();
+      Date.prototype.getTimezoneOffset = function() { return offset; };
+      scope.apply(this, arguments);
+    } finally {
+      offset = 0;
+      Date.prototype.getTimezoneOffset = o;
+    }
+  };
+};
